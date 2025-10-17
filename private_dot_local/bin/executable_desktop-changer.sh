d7d9e07cd66beaf76feed87c47509a6a37587ba5
Lorenzo Drumond <drumondlorenzo@posteo.net>
2025-10-17 11:43:49 +0100

    chore: commit save point

--- private_dot_local/bin/executable_desktop-changer.sh
+++ private_dot_local/bin/executable_desktop-changer.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-path=$( find /home/ld/Pictures/Images/Current\ Wallpapers/ -maxdepth 1 -type f | shuf -n 1 )
-uri="file://$path"
+path="$HOME/Pictures/Images/Current Wallpapers"
+name=$( ls -t "$path" | tail -n 5 | shuf -n 1 )
+fullpath="$path/$name"
+touch "$fullpath"
+uri="file://$fullpath"
 
 gsettings set org.gnome.desktop.background picture-uri "$uri"
 gsettings set org.gnome.desktop.background picture-uri-dark "$uri"
