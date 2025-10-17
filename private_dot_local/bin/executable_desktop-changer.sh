004464501ce27ec69d69cc760c24302f81601124
Lorenzo Drumond <drumondlorenzo@posteo.net>
2025-10-17 10:33:13 +0100

    chore: commit save point

--- private_dot_local/bin/executable_desktop-changer.sh
+++ private_dot_local/bin/executable_desktop-changer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-path=$( find /home/ld/Pictures/Images/Wallpapers/ -maxdepth 1 -type f | shuf -n 1 )
+path=$( find /home/ld/Pictures/Images/Current\ Wallpapers/ -maxdepth 1 -type f | shuf -n 1 )
 uri="file://$path"
 
 gsettings set org.gnome.desktop.background picture-uri "$uri"
